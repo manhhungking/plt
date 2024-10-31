10f5e90bc0a06f1b3f60d0c7e85009f386d915f4
manhhungking <trinhmanhhung03@gmail.com>
2024-10-31 17:16:04 +0200

    Test User story 3 - translating a word starting with a vowel

--- piglatin.py
+++ piglatin.py
@@ -9,6 +9,15 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase == "":
             return "nil"
+        the_vowel = ["a", "e", "i", "o", "u"]
+        if self.phrase.lower()[0] in the_vowel:
+            if self.phrase.lower()[-1] == "y":
+                return self.phrase + "nay"
+            elif self.phrase.lower()[-1] in the_vowel:
+                return self.phrase + "yay"
+            else:
+                return self.phrase + "ay"
+
 
     def PigLatinTranslator(phrase):
         return PigLatin(phrase)
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -18,3 +18,18 @@ class TestPigLatin(unittest.TestCase):
         translator = PigLatin.PigLatinTranslator("")
         translate = translator.translate()
         self.assertEqual("nil", translate)
+
+    def test_translating_word_start_with_a_vowel_0(self):
+        translator = PigLatin.PigLatinTranslator("any")
+        translate = translator.translate()
+        self.assertEqual("anynay", translate)
+
+    def test_translating_word_start_with_a_vowel_1(self):
+        translator = PigLatin.PigLatinTranslator("apple")
+        translate = translator.translate()
+        self.assertEqual("appleyay", translate)
+
+    def test_translating_word_start_with_a_vowel_2(self):
+        translator = PigLatin.PigLatinTranslator("ask")
+        translate = translator.translate()
+        self.assertEqual("askay", translate)
